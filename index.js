17e7c17389262f458873bd778ce7e5a8f66a1926
Garret <gwiats@gmail.com>
2021-03-31 01:33:52 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Add your code here
+// fetch("http://localhost:3000/dogs", {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//     "Accept": "application/json"
+//   },
+//   body: JSON.stringify({
+//     dogName: "Byron",
+//     dogBreed: "Poodle"
+//   })
+// }); 
+
+
+function submitData (nameVal, emailVal) {
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            name: `${nameVal}`,
+            email: `${emailVal}`
+        })
+    })
+    .then(res => res.json())
+    .then(value => {
+        //console.log(value)
+        const loc = document.createElement('div')
+        loc.innerHTML = value.id
+        document.body.appendChild(loc)
+    })
+    .catch(value => {
+        alert('SKY, IS, FALLING')
+        const loc = document.createElement('div')
+        loc.innerHTML = value.message
+        document.body.appendChild(loc)
+    })
+}
